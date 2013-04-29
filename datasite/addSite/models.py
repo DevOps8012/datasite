261b8a29ecf6374288df282d30b77e3681693050
lenwood <lenwood.cc@gmail.com>
2013-04-28 20:18:43 -0500

    setup for accounts

--- datasite/addSite/models.py
+++ datasite/addSite/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class webSite(models.Model):
+	userId = models.ForeignKey(User)
+	accountId = models.CharField(max_length=30)
+	webPropertyId = models.CharField(max_length=4)
+
+class dataSource(models.Model):
+	webSiteId = models.ForeignKey(webSite)
+	name = models.Model(max_length=90)
+	description = models.Model(max_length=300)
+	customDataSourceId = models.Model(max_length=35)
